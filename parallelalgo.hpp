d47ee6e9a6846f2a2c86ba7824c2d423c7f52d26
Robert burner Schadek <rburners@gmail.com>
2013-01-21 01:06:13 +0100

    transform

--- parallelalgo.hpp
+++ parallelalgo.hpp
@@ -4,10 +4,11 @@
 #include <algorithm>
 #include <vector>
 #include <thread>
+#include <mutex>
 
 namespace css {
 
-template<class Iterator, class UnaryFunction>
+template<typename Iterator, typename UnaryFunction>
 UnaryFunction for_each_impl(Iterator first, Iterator second,
 		UnaryFunction f, std::random_access_iterator_tag, size_t numThreads) {
 
@@ -30,13 +31,50 @@ UnaryFunction for_each_impl(Iterator first, Iterator second,
 	return f;
 }
 
-template<class Iterator, class UnaryFunction>
+template<typename Iterator, typename UnaryFunction>
 UnaryFunction for_each(Iterator first, Iterator second, UnaryFunction f,
 		size_t numThreads = 2) {
 	typedef typename std::iterator_traits<Iterator>::iterator_category category;
 	return for_each_impl(std::forward<Iterator>(first), 
 		std::forward<Iterator>(second), std::forward<UnaryFunction>(f),
 		category(), numThreads);
+}
+
+template<typename Iterator, typename OIterator, typename UnaryFunction>
+OIterator transform_impl(Iterator first, Iterator second, OIterator out, 
+		UnaryFunction f, size_t numThreads, std::random_access_iterator_tag, 
+		std::random_access_iterator_tag) {
+	std::vector<std::thread> threads;
+	const size_t dist = std::distance(first, second);
+	const size_t stepWidth = dist/2u;
+
+	for(size_t i = 0; i < numThreads; ++i) {
+		int fillUp = (i+1 == numThreads) && (dist % 2 != 0) ? 1 : 0;
+		threads.push_back(
+			std::thread(
+				std::transform<Iterator, OIterator, UnaryFunction>,
+				first+(i*stepWidth), first+((i+1)*stepWidth) + fillUp,
+				out+(i*stepWidth), f
+			)
+		);
+	}
+
+	for(size_t i = 0; i < numThreads; ++i) {
+		threads[i].join();
+	}
+
+	return out;
+}
+
+template<typename Iterator, typename OIterator, typename UnaryFunction>
+OIterator transform(Iterator first, Iterator second, OIterator out, 
+		UnaryFunction f, size_t numThreads = 2) {
+	typedef typename std::iterator_traits<Iterator>::iterator_category iType;
+	typedef typename std::iterator_traits<OIterator>::iterator_category oType;
+
+	return transform_impl(std::forward<Iterator>(first),
+			std::forward<Iterator>(second), std::forward<OIterator>(out),
+			std::forward<UnaryFunction>(f), numThreads, iType(), oType());
 }
 
 }
