ecf6269f6f5ba5e727809b3ae82f3976cbb4c69a
Robert burner Schadek <robert.schadek@informatik.uni-oldenburg.de>
2013-06-11 21:09:40 +0200

    some more

--- benchmarks/strcmpsse.cpp
+++ benchmarks/strcmpsse.cpp
@@ -4,8 +4,8 @@
 #include <logger.hpp>
 #include <algorithm>
 #include <benchmark.hpp>
+#include "smmintrin.h"
 
-/*
 int sse_strcmp(const char *p1, const char *p2)  {
 	const int mode = _SIDD_UBYTE_OPS | _SIDD_CMP_EQUAL_EACH | _SIDD_BIT_MASK | _SIDD_NEGATIVE_POLARITY;
 	__m128i smm1 = _mm_loadu_si128 ((__m128i *) p1);
@@ -21,16 +21,19 @@ int sse_strcmp(const char *p1, const char *p2)  {
 	}
 	p1 = (char *) & smm1;
 	p2 = (char *) & smm2;
-	if(p1[ResultIndex]<p2[ResultIndex])  return ‐1;
-	if(p1[ResultIndex]>p2[ResultIndex])  return 1;
+	if(p1[ResultIndex] < p2[ResultIndex]) return -1;
+	if(p1[ResultIndex] > p2[ResultIndex]) return 1;
 	return 0;
-}*/
+}
 
 int main() {
 	std::ifstream infile("text3");
 	std::vector<std::string> words(10000);
 	std::string line;
 	while (std::getline(infile, line)) {
+		if(line.empty() && line == "") {
+			continue;
+		}
 		words.push_back(line);
 	}
 
@@ -57,7 +60,7 @@ int main() {
 	Bench sse;
 	for(size_t i = 0; i < wordsSize; ++i) {
 		for(size_t j = 0; j < wordsSize; ++j) {
-			if(words[i] == shuffle[j]) {
+			if(sse_strcmp(words[i].c_str(), shuffle[j].c_str())) {
 				ws[i] = j;
 				break;
 			}
