a0670a63b284328988e8f8b68446c10c3cf44f60
Robert burner Schadek <rburners@gmail.com>
2013-06-17 13:10:54 +0200

    made it compile with clang

--- parallelalgo.hpp
+++ parallelalgo.hpp
@@ -11,11 +11,11 @@
 
 namespace css {
 
-unsigned getNumberOfCores() {
+inline unsigned getNumberOfCores() {
 	return sysconf(_SC_NPROCESSORS_ONLN);
 }
 
-template<typename Iterator, typename UnaryFunction>
+inline template<typename Iterator, typename UnaryFunction>
 UnaryFunction for_each_impl(Iterator first, Iterator second,
 		UnaryFunction f, std::random_access_iterator_tag, size_t numThreads) {
 
@@ -38,7 +38,7 @@ UnaryFunction for_each_impl(Iterator first, Iterator second,
 	return f;
 }
 
-template<typename Iterator, typename UnaryFunction>
+inline template<typename Iterator, typename UnaryFunction>
 UnaryFunction for_each(Iterator first, Iterator second, UnaryFunction f,
 		size_t numThreads = getNumberOfCores()) {
 	typedef typename std::iterator_traits<Iterator>::iterator_category category;
@@ -47,7 +47,7 @@ UnaryFunction for_each(Iterator first, Iterator second, UnaryFunction f,
 		category(), numThreads);
 }
 
-template<typename Iterator, typename OIterator, typename UnaryFunction>
+inline template<typename Iterator, typename OIterator, typename UnaryFunction>
 OIterator transform_impl(Iterator first, Iterator second, OIterator out, 
 		UnaryFunction f, size_t numThreads, std::random_access_iterator_tag, 
 		std::random_access_iterator_tag) {
@@ -73,7 +73,7 @@ OIterator transform_impl(Iterator first, Iterator second, OIterator out,
 	return out;
 }
 
-template<typename Iterator, typename OIterator, typename UnaryFunction>
+inline template<typename Iterator, typename OIterator, typename UnaryFunction>
 OIterator transform(Iterator first, Iterator second, OIterator out, 
 		UnaryFunction f, size_t numThreads = getNumberOfCores()) {
 	typedef typename std::iterator_traits<Iterator>::iterator_category iType;
@@ -84,7 +84,7 @@ OIterator transform(Iterator first, Iterator second, OIterator out,
 			std::forward<UnaryFunction>(f), numThreads, iType(), oType());
 }
 
-template<typename Iterator, typename OIterator, typename UnaryFunction>
+inline template<typename Iterator, typename OIterator, typename UnaryFunction>
 UnaryFunction transform(Iterator first, Iterator second, OIterator oit,
 		UnaryFunction f, size_t numThreads = 2) {
 	typedef typename std::iterator_traits<Iterator>::iterator_category category;
@@ -94,31 +94,33 @@ UnaryFunction transform(Iterator first, Iterator second, OIterator oit,
 		std::forward<UnaryFunction>(f), category(), numThreads);
 }
 
-template<typename Iterator, typename OIterator, typename Tmp, typename UnaryFunction>
+inline template<typename Iterator, typename OIterator, typename Tmp, typename UnaryFunction>
 OIterator mapReduce(Iterator first, Iterator second, OIterator out, 
 		UnaryFunction f, size_t numThreads = getNumberOfCores()) {
 	struct Job {
-		Iterator b, e;
+		Iterator b;
+	    Iterator e;
 		OIterator outIter;
 		Tmp tmpStore;
 		UnaryFunction func;
 		std::mutex& jmutex;
 
-		Job(Iterator nb, Iterator ne, OIterator nOutIter, UnaryFunction nFunc,
+		inline Job(Iterator nb, Iterator ne, OIterator nOutIter, UnaryFunction nFunc,
 				std::mutex& nJmutex) : b(nb), e(ne), outIter(nOutIter),
 				func(nFunc), jmutex(nJmutex) {
 		}
 
-		void operator()() {
-			std::insert_iterator<Tmp> tmpOut = std::inserter(tmpStore,
-					tmpStore.end());
-			for(; b != e; ++b) {
-				func(*b, tmpOut);	
+		inline void operator()() {
+			/*std::insert_iterator<Tmp> tmpOut = std::inserter(tmpStore,
+					tmpStore.end());*/
+			auto tmpOut = std::inserter(this->tmpStore, this->tmpStore.end());
+			for(; this->b != this->e; ++this->b) {
+				this->func(*this->b, tmpOut);	
 			}
 
-			jmutex.lock();
-			std::copy(tmpStore.begin(), tmpStore.end(), outIter);
-			jmutex.unlock();
+			this->jmutex.lock();
+			std::copy(this->tmpStore.begin(), this->tmpStore.end(), this->outIter);
+			this->jmutex.unlock();
 		}
 	};
 
