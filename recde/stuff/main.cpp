828d25178d46aae159c02f0682d5b9f5ee13351c
Robert burner Schadek <robert.schadek@informatik.uni-oldenburg.de>
2013-11-08 22:36:29 +0100

    forgot one

---
+++ recde/stuff/main.cpp
@@ -0,0 +1,18 @@
+#include <parser.hpp>
+#include <logger.hpp>
+#include <unit.hpp>
+
+UNITTEST(unary1) {
+	auto ss = std::make_shared<std::stringstream>
+		("hello");
+	Lexer l(ss);
+	Parser p(l);
+	auto ast = p.parseStart();
+	ast->toOutStream(std::cout);
+	std::cout<<std::endl;
+}
+
+int main() {
+	Unit::runTests();
+	return 0;
+}
