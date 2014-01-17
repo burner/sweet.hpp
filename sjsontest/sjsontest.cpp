e0bc4905a0e3d3191af689d9ca4c5e8a66185d26
Robert BuRnEr Schadek <rburners@gmail.com>
2014-01-17 16:37:05 +0100

    another sjson example

--- sjsontest/sjsontest.cpp
+++ sjsontest/sjsontest.cpp
@@ -1,18 +1,23 @@
 #include <iostream>
 #include <sjson.hpp>
-//#include <unit.hpp>
+#include <unit.hpp>
 
-int main() {
-	//if(!Unit::runTests()) {
-	//	std::cout<<"found error exiting program"<<std::endl;
-	//	return 1;
-	//}
+UNITTEST(test1) {
 	sweet::jsonparser t("hull.js");
 	auto r = t.getRoot();
+	AS_EQ(r->access("Config.system.numNodes")->getInt(), 32);
+}
+
+int main() {
+	unsigned c = sweet::Unit::runTests();
+	if(c) {
+		std::cout<<"found error exiting program "<<c<<std::endl;
+		return 1;
+	}
 	//std::cout<<t;
 
 	sweet::jsonparser t2("hull2.js");
-	r = t2.getRoot();
+	auto r = t2.getRoot();
 	std::cout<<t2;
 
 	return 0;
