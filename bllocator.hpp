4864bbfbbb37b8baf4747b7b5c19a913a94b0f12
Robert burner Schadek <rburners@gmail.com>
2014-06-26 17:18:27 +0200

    bllocator rock

--- bllocator.hpp
+++ bllocator.hpp
@@ -3,33 +3,229 @@
 #include <stdio.h>
 #include <stddef.h>
 #include <limits>
+#include <algorithm>
+#include <deque>
 #include <memory>
 
 namespace sweet {
 	class BaseAllocator {
 	  public:
-		template<typename U, typename... Args>
-		void construct(U* p, Args&&... args) {
-			::new((void*)p) U(std::forward<Args>(args)...);
-		}
-
-		template<typename U>
-		void destroy(U* p) {
-			p->~T();
-		}
-
 		virtual void* allocate(size_t) = 0;
 		virtual void deallocate(void*, size_t=0) = 0;
+		virtual size_t max() const = 0;
+		virtual bool allocated(void*) const = 0;
 	};
 
 	class Mallocator : public BaseAllocator {
 	  public:
-		void* allocate(size_t s) override {
+		inline void* allocate(size_t s) override {
 			return malloc(s);
 		}
 
-		void deallocate(void* ptr, size_t=0) override {
+		inline void deallocate(void* ptr, size_t=0) override {
 			free(ptr);
+		}
+
+		inline size_t max() const override {
+			return std::numeric_limits<size_t>::max();
+		}
+
+		inline bool allocated(void*) const override {
+			return true;
+		}
+	};
+
+	class FailAllocator : public BaseAllocator {
+	  public:
+		inline void* allocate(size_t) override {
+			return nullptr;
+		}
+
+		inline void deallocate(void*, size_t) override {
+		}
+
+		inline size_t max() const {
+			return 0u;
+		}
+
+		inline bool allocated(void*) const override {
+			return false;
+		}
+	};
+
+	template<typename A1, typename A2>
+	class FallbackAllocator : public BaseAllocator {
+	  private:
+		A1 a1;
+		A2 a2;
+
+	  public:
+		inline void* allocate(size_t s) override {
+			void* ptr = a1.allocate(s);
+			if(ptr == nullptr) {
+				ptr = a2.allocate(s);
+			}
+
+			if(ptr == nullptr) {
+				throw std::bad_alloc();
+			} else {
+				return ptr;
+			}
+		}
+
+		inline void deallocate(void* ptr, size_t s) override {
+			if(a1.allocated(ptr)) {
+				a1.deallocate(ptr, s);
+			} else if(a2.allocated(ptr)) {
+				a2.deallocate(ptr, s);
+			} else {
+				throw std::logic_error("Either of the two Allocated allocated "
+					"the given ptr.");
+			}
+		}
+
+		inline size_t max() const {
+			return std::max(a1.max(), a2.max());
+		}
+
+		inline bool allocated(void* ptr) const override {
+			if(a1.allocated(ptr)) {
+				return true;
+			} else if(a2.allocated(ptr)) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+	};
+
+	template<typename A>
+	class FreeDequeAllocator : public BaseAllocator {
+	  private:
+		struct StoreNode {
+			void* ptr;
+			size_t size;
+
+			inline StoreNode(void* p, size_t s) : ptr(p), size(s) {}
+		};
+		std::deque<StoreNode> store;
+
+		A allocator;
+
+	  public:
+		inline ~FreeDequeAllocator() {
+			std::for_each(this->store.begin(), this->store.end(), [&](const StoreNode& n) {
+				allocator.deallocate(n.ptr, n.size);
+			});
+		}
+
+		inline void* allocate(size_t size) override {
+			auto it = std::find_if(store.begin(), store.end(), 
+					[&](const typename std::deque<StoreNode>::value_type& s) {
+				return s.size == size;
+			});
+
+			if(it != this->store.end()) {
+				void* ptr = it->ptr;
+				this->store.erase(it);
+				return ptr;
+			} else {
+				return this->allocator.allocate(size);
+			}
+		}
+
+		inline void deallocate(void* p, size_t s) override {
+			this->store.push_back(StoreNode(p,s));
+		}
+
+		inline size_t max() const override {
+			return this->allocator.max();
+		}
+
+		inline bool allocated(void* p) const override {
+			return this->allocator.allocated(p);
+		}
+	};
+
+	template<size_t Size = 4096>
+	class StackAllocator : public BaseAllocator {
+	  private:
+		char stack[Size];
+		size_t idx;
+
+	  public:
+		inline StackAllocator() : idx(0u) {}
+
+		inline void* allocate(size_t size) override {
+			size_t nIdx = this->idx + size;
+			if(nIdx > Size) {
+				return nullptr;
+			} else {
+				void* ret = reinterpret_cast<void*>(&this->stack[this->idx]);
+				this->idx = nIdx;
+				return ret;
+			}
+		}
+
+		inline void deallocate(void* p, size_t s) override {
+			char* a = static_cast<char*>(p) + s;
+			char* b = &(this->stack[this->idx]);
+			if(a == b) {
+				this->idx -= s;
+			}
+		}
+
+		inline size_t max() const override {
+			return Size;
+		}
+
+		inline bool allocated(void* p) const override {
+			return p >= &this->stack && p <= &this->stack[this->idx];
+		}
+	};
+
+	template<size_t Size = 4096>
+	class PoolAllocator : public BaseAllocator {
+	  private:
+		char* stack;
+		size_t idx;
+
+	  public:
+		inline PoolAllocator() : stack(static_cast<char*>(malloc(Size))), idx(0u) {
+			if(this->stack == nullptr) {
+				throw std::bad_alloc();
+			}
+		}
+
+		inline ~PoolAllocator() {
+			free(this->stack);
+		}
+
+		inline void* allocate(size_t size) override {
+			size_t nIdx = this->idx + size;
+			if(nIdx > Size) {
+				return nullptr;
+			} else {
+				void* ret = reinterpret_cast<void*>(&this->stack[this->idx]);
+				this->idx = nIdx;
+				return ret;
+			}
+		}
+
+		inline void deallocate(void* p, size_t s) override {
+			char* a = static_cast<char*>(p) + s;
+			char* b = &(this->stack[this->idx]);
+			if(a == b) {
+				this->idx -= s;
+			}
+		}
+
+		inline size_t max() const override {
+			return Size;
+		}
+
+		inline bool allocated(void* p) const override {
+			return p >= this->stack && p <= &this->stack[this->idx];
 		}
 	};
 
@@ -46,8 +242,10 @@ namespace sweet {
 		typedef const T& const_reference;
 		typedef T value_type;
 
+		inline STLAllo() {}
+
 		template<typename U>
-		class rebind {typedef STLAllo<U,Allo> other;};
+		struct rebind {typedef STLAllo<U,Allo> other;};
 
 		inline pointer address(reference r) const noexcept {
 			return &r;
@@ -58,7 +256,7 @@ namespace sweet {
 		}
 
 		inline pointer allocate(size_type n, const void* =0) {
-			auto ptr = allo.allocate(n);
+			auto ptr = allo.allocate(sizeof(T) * n);
 			if(ptr == nullptr) {
 				throw std::bad_alloc();
 			}
@@ -67,11 +265,31 @@ namespace sweet {
 		}
 
 		inline void deallocate(pointer ptr, size_type s) {
-			this->allo.deallocate(ptr, s);
+			this->allo.deallocate(ptr, sizeof(T) * s);
 		}
 
 		inline size_type max_size() const {
 			return this->allo.max();
 		}
+
+		template<typename... Args>
+		void construct(pointer p, Args&&... args) {
+			::new((void*)p) T(std::forward<Args>(args)...);
+		}
+
+		void destroy(pointer p) {
+			p->~T();
+		}
+
 	};
+
+	template <class T1, class T2, class A1, class A2>
+	bool operator==(const STLAllo<T1,A1>&, const STLAllo<T2,A2>&) {
+		return std::is_same<A1,A2>::value;
+	}
+ 
+	template <class T1, class T2, class A1, class A2>
+	bool operator!=(const STLAllo<T1,A1>&, const STLAllo<T2,A2>&) {
+		return !std::is_same<A1,A2>::value;
+	}
 }
--- bllocatortest/allocatortest.cpp
+++ bllocatortest/allocatortest.cpp
@@ -13,9 +13,279 @@ UNITTEST(test1) {
 	m.deallocate(ptr);
 }
 
+UNITTEST(test2) {
+	typedef sweet::STLAllo<int,sweet::Mallocator> IntAllo;
+
+	std::vector<int, IntAllo> v1;
+	for(int i = 0; i < 100; ++i) {
+		v1.push_back(i);
+		for(int j = 0; j < i; ++j) {
+			AS_EQ(v1[j], j);
+		}
+	}
+
+	std::map<int,int,std::less<int>,IntAllo> m1;
+	const int cnt = 100;
+	const int step = 2;
+	for(int j = 0; j < cnt; j += step) {
+		for(int i = 0; i < j; ++i) {
+			m1.insert(std::make_pair(i,i));
+			auto it = m1.find(i);
+			AS_T(it != m1.end());
+		}
+
+		for(int i = 0; i < j/2u; ++i) {
+			auto it = m1.find(i);
+			AS_T(it != m1.end());
+			m1.erase(it);
+		}
+	}
+}
+
+UNITTEST(test3) {
+	typedef sweet::STLAllo<int, 
+		sweet::FallbackAllocator<
+			sweet::FailAllocator,
+			sweet::Mallocator
+		>
+	> IntAllo;
+
+	std::vector<int, IntAllo> v1;
+	for(int i = 0; i < 100; ++i) {
+		v1.push_back(i);
+		for(int j = 0; j < i; ++j) {
+			AS_EQ(v1[j], j);
+		}
+	}
+
+	std::map<int,int,std::less<int>,IntAllo> m1;
+	const int cnt = 100;
+	const int step = 2;
+	for(int j = 0; j < cnt; j += step) {
+		for(int i = 0; i < j; ++i) {
+			m1.insert(std::make_pair(i,i));
+			auto it = m1.find(i);
+			AS_T(it != m1.end());
+		}
+
+		for(int i = 0; i < j/2u; ++i) {
+			auto it = m1.find(i);
+			AS_T(it != m1.end());
+			m1.erase(it);
+		}
+	}
+}
+
+
+UNITTEST(test4) {
+	typedef sweet::STLAllo<int, 
+		sweet::FallbackAllocator<
+			sweet::StackAllocator<4096>,
+			sweet::Mallocator
+		>
+	> IntAllo;
+
+	std::vector<int, IntAllo> v1;
+	for(int i = 0; i < 100; ++i) {
+		v1.push_back(i);
+		for(int j = 0; j < i; ++j) {
+			AS_EQ(v1[j], j);
+		}
+	}
+
+	std::map<int,int,std::less<int>,IntAllo> m1;
+	const int cnt = 100;
+	const int step = 2;
+	for(int j = 0; j < cnt; j += step) {
+		for(int i = 0; i < j; ++i) {
+			m1.insert(std::make_pair(i,i));
+			auto it = m1.find(i);
+			AS_T(it != m1.end());
+		}
+
+		for(int i = 0; i < j/2u; ++i) {
+			auto it = m1.find(i);
+			AS_T(it != m1.end());
+			m1.erase(it);
+		}
+	}
+}
+
+UNITTEST(test4_5) {
+	sweet::StackAllocator<256> s;
+	for(int i = 0; i < 280; ++i) {
+		void* a = s.allocate(1);
+		if(i < 256) {
+			AS_T(a != nullptr);
+			AS_T_C(s.allocated(a), [&]() {
+				LOG("%d", i);
+			});
+		} else {
+			AS_T(a == nullptr);
+			AS_F(s.allocated(a));
+		}
+	}
+}
+
+UNITTEST(test4_10) {
+	sweet::PoolAllocator<256> s;
+	for(int i = 0; i < 280; ++i) {
+		void* a = s.allocate(1);
+		if(i < 256) {
+			AS_T(a != nullptr);
+			AS_T_C(s.allocated(a), [&]() {
+				LOG("%d", i);
+			});
+		} else {
+			AS_T(a == nullptr);
+			AS_F(s.allocated(a));
+		}
+	}
+}
+
+UNITTEST(test5) {
+	typedef sweet::STLAllo<int, 
+		sweet::FallbackAllocator<
+			sweet::FreeDequeAllocator<sweet::PoolAllocator<4096>>,
+			sweet::Mallocator
+		>
+	> IntAllo;
+
+	std::vector<int, IntAllo> v1;
+	for(int i = 0; i < 100; ++i) {
+		v1.push_back(i);
+		for(int j = 0; j < i; ++j) {
+			AS_EQ(v1[j], j);
+		}
+	}
+
+	std::map<int,int,std::less<int>,IntAllo> m1;
+	const int cnt = 100;
+	const int step = 2;
+	for(int j = 0; j < cnt; j += step) {
+		for(int i = 0; i < j; ++i) {
+			m1.insert(std::make_pair(i,i));
+			auto it = m1.find(i);
+			AS_T(it != m1.end());
+		}
+
+		for(int i = 0; i < j/2u; ++i) {
+			auto it = m1.find(i);
+			AS_T(it != m1.end());
+			m1.erase(it);
+		}
+	}
+}
+
+template<typename T>
+void fillVectorDeque(size_t cnt) {
+	//format(std::cout, "%u\n", cnt);
+	T v1;
+	size_t step = cnt / 10u;
+	for(size_t j = step; j < cnt; j += step) {
+		for(size_t i = 0; i < j; ++i) {
+			v1.push_back(i);
+			if(v1.back() != i) {
+				format(std::cout, "%u != %u", v1.back(), i);
+				throw std::exception();
+			}
+		}
+
+		for(size_t i = 0; i < j/2u; ++i) {
+			v1.pop_back();
+		}
+	}
+}
+
+template<typename T>
+void fillMap(size_t cnt) {
+	//format(std::cout, "%u\n", cnt);
+	T v1;
+	size_t step = cnt / 10u;
+	for(size_t j = step; j < cnt; j += step) {
+		for(size_t i = 0; i < j; ++i) {
+			v1.insert(std::make_pair(i,i));
+		}
+
+		for(size_t i = 0; i < j/2u; ++i) {
+			auto it = v1.find(i);
+			v1.erase(it);
+		}
+	}
+}
+
 int main() {
 	unsigned worked = sweet::Unit::runTests();
 	std::cout<<worked <<" Number of Asserts "<<
 		sweet::Unit::getNumOfAsserts()<<std::endl;
+
+	LOG("vector");
+	//std::vector<size_t> values {10,24,32,64,128,256,512};
+	std::vector<size_t> values {10,24,32,64};
+	size_t cnt = 5500;
+	for(size_t i : values) {
+		double a = BENCHMARK_CNT(cnt, (fillVectorDeque<std::vector<size_t>>(i)));
+		double b = BENCHMARK_CNT(cnt, (fillVectorDeque<std::vector<size_t,sweet::STLAllo<size_t,sweet::Mallocator>>>(i)));
+
+		typedef std::vector<size_t, 
+			sweet::STLAllo<
+				size_t,
+				sweet::FallbackAllocator<
+					sweet::StackAllocator<4096>,
+					sweet::Mallocator
+				>
+			>
+		> StackVector;
+		
+		double c = BENCHMARK_CNT(cnt, fillVectorDeque<StackVector>(i));
+
+		typedef std::vector<size_t, 
+			sweet::STLAllo<
+				size_t,
+				sweet::FallbackAllocator<
+					sweet::PoolAllocator<4096>,
+					sweet::Mallocator
+				>
+			>
+		> PoolVector;
+		
+		double d = BENCHMARK_CNT(cnt, fillVectorDeque<PoolVector>(i));
+
+		format(std::cout, "%10u: %9.4f %9.4f %9.4f %9.4f\n", i, a, b, c, d);
+	}
+
+	LOG("\nmap");
+	for(size_t i : values) {
+		double a = BENCHMARK_CNT(cnt, (fillMap<std::map<size_t,size_t>>(i)));
+		double b = BENCHMARK_CNT(cnt, (fillMap<std::map<size_t,size_t, std::less<size_t>,
+			sweet::STLAllo<std::pair<size_t,size_t>,sweet::Mallocator>>>(i)));
+
+		typedef std::map<size_t,size_t,std::less<size_t>,
+			sweet::STLAllo<
+				std::pair<size_t,size_t>,
+				sweet::FallbackAllocator<
+					sweet::StackAllocator<4096>,
+					sweet::Mallocator
+				>
+			>
+		> StackMap;
+		
+		double c = BENCHMARK_CNT(cnt, fillMap<StackMap>(i));
+
+		typedef std::map<size_t,size_t,std::less<size_t>,
+			sweet::STLAllo<
+				std::pair<size_t,size_t>,
+				sweet::FallbackAllocator<
+					sweet::PoolAllocator<4096>,
+					sweet::Mallocator
+				>
+			>
+		> PoolMap;
+		
+		double d = BENCHMARK_CNT(cnt, fillMap<PoolMap>(i));
+
+		format(std::cout, "%10u: %9.4f %9.4f %9.4f %9.4f\n", i, a, b, c, d);
+	}
+
 	return 0;
 }
