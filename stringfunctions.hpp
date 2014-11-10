1ccba1413a9272ede9d335e43c661de9620bc218
Robert burner Schadek <rburners@gmail.com>
2014-11-10 15:58:05 +0100

    some more

--- stringfunctions.hpp
+++ stringfunctions.hpp
@@ -34,4 +34,22 @@ V split(const std::string& s, char delim = ',') {
     return std::move(elems);
 }
 
+// from http://stackoverflow.com/questions/216823/whats-the-best-way-to-trim-stdstring
+// trim from start
+inline std::string &ltrim(std::string &s) {
+	s.erase(s.begin(), std::find_if(s.begin(), s.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
+	return s;
+}
+
+// trim from end
+inline std::string &rtrim(std::string &s) {
+	s.erase(std::find_if(s.rbegin(), s.rend(), std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
+	return s;
+}
+
+// trim from both ends
+inline std::string &trim(std::string &s) {
+	return ltrim(rtrim(s));
+}
+
 }
