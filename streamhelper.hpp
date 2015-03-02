75fe283764cdf818baa05a5e3f475d7017bf2661
Robert burner Schadek <rburners@gmail.com>
2015-03-02 11:43:39 +0100

    stream helper

---
+++ streamhelper.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <ifstream>
+
+namespace sweet {
+int checkErrorBits(ifstream& f) {
+	int stop = 0;
+	if (f.eof()) {
+		perror("stream eofbit. error state");
+		// EOF after std::getline() is not the criterion to stop processing
+		// data: In case there is data between the last delimiter and EOF,
+		// getline() extracts it and sets the eofbit.
+		stop = 0;
+	}
+	if (f.fail()) {
+		perror("stream failbit (or badbit). error state");
+		stop = 1;
+	}
+	if (f.bad()) {
+		perror("stream badbit. error state");
+		stop = 1;
+	}
+	return stop;
+}
+} // ~namespace sweet
